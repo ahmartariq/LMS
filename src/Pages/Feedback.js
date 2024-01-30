e021466cb0b921428f6962c14278ac01a1d55f8d
ahmartariq <ahmartariq46@yahoo.com>
2024-01-30 16:12:43 +0500

    completed

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -5,15 +5,20 @@ import { useState } from "react";
 
 export const Feedback = () => {
     const { module } = useParams();
-    const [answer, setAnswer] = useState('')
+    const [stdFeedback, setStdFeedback] = useState('')
+    const [tutorFeedback, setTutorFeedback] = useState('')
 
 
     const found = modules.find((mod) => {
         if (module === mod.name) return mod;
     });
 
-    const handleAnswer = event => {
-        setAnswer(event.target.value);
+    const handleStd = event => {
+        setStdFeedback(event.target.value);
+    };
+
+    const handleTutor = event => {
+        setTutorFeedback(event.target.value);
     };
 
 
@@ -22,14 +27,13 @@ export const Feedback = () => {
 
         return (
             <div className="container flex flex-col justify-center items-center mt-8 pb-12">
-                <h1 className="font-bold text-4xl primary-color">Feedback Tab</h1>
-                <div className="w-full  md:w-4/5 mt-12 rounded-md primary-bg px-6 py-9 text-white flex flex-row gap-5 items-center shadow-lg" >
+                <div className="w-full  md:w-4/5 mt-12 rounded-md primary-bg px-6 py-9 text-white flex flex-row gap-3 items-center shadow-lg" >
                     <div className="circle-feedback">
                         <p className="font-bold text-lg text-center">Student Feedback</p>
                     </div>
                     <input
                         className="appearance-none rounded-none relative block w-3/4 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm max-h-11"
-                        type="text" value={answer} onChange={handleAnswer} placeholder="Feedback" />
+                        type="text" value={stdFeedback} onChange={handleStd} placeholder="Student Feedback" readOnly/>
                 </div>
 
                 <div className="w-full  md:w-4/5 mt-12 rounded-md primary-bg px-6 py-9 text-white flex flex-row gap-3 items-center shadow-lg" >
@@ -38,7 +42,7 @@ export const Feedback = () => {
                     </div>
                     <input
                         className="appearance-none rounded-none relative block w-3/4 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm max-h-11"
-                        type="text" value={answer} onChange={handleAnswer} placeholder="Feedback" />
+                        type="text" value={tutorFeedback} onChange={handleTutor} placeholder="Tutor Feedback" readOnly/>
                 </div>
             </div>
         )
@@ -46,7 +50,6 @@ export const Feedback = () => {
     else {
         return (
             <div className="container flex flex-col justify-center items-center mt-8 pb-12">
-                <h1 className="font-bold text-4xl primary-color">Feedback Tab</h1>
                 <div className="w-full md:w-1/2 mt-12 rounded-md primary-bg px-6 py-9 text-white flex flex-row gap-5 justify-center items-center shadow-lg" >
                     <h1 className="font-bold text-lg">You cannot give feedback right now!</h1>
                 </div>
